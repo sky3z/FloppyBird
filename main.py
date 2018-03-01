ab7ebe46a237e9ee3a265476d6d70a637fed1eb2
root <root@localhost.localdomain>
2018-03-01 22:26:52 +0100

    0.1.1 Add settings file

--- main.py
+++ main.py
@@ -3,22 +3,12 @@ import pygame
 import random
 import time
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
