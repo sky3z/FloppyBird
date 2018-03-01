88622e3822eec5bf392ffe0c4e5884a48845aab9
root <root@localhost.localdomain>
2018-03-01 22:35:48 +0100

    0.2 Add Class FloppyBird

--- main.py
+++ main.py
@@ -2,35 +2,39 @@
 import pygame
 import random
 import time
+from settings import *
 
 
-# initialize pygame and create window
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption(TITLE)
-clock = pygame.time.Clock()
+class FloppyBird():
+    # initialize pygame and create window
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
 
-all_sprites = pygame.sprite.Group()
-# Game loop
-running = True
-while running:
-    # keep loop running at the right speed
-    clock.tick(FPS)
-    # Process input (events)
-    for event in pygame.event.get():
-        # check for closing window
-        if event.type == pygame.QUIT:
-            time.sleep(1)
-            running = False
+    def run(self):
+        self.all_sprites = pygame.sprite.Group()
+        # Game loop
+        self.running = True
+        while self.running:
+            # keep loop running at the right speed
+            self.clock.tick(FPS)
+            # Process input (events)
+            for event in pygame.event.get():
+                # check for closing window
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-    # Update
-    all_sprites.update()
-    # Draw / render
-    screen.fill(BLUE)
-    all_sprites.draw(screen)
+            # Update
+            self.all_sprites.update()
+            # Draw / render
+            self.screen.fill(BLUE)
+            self.all_sprites.draw(screen)
 
-    # *after* drawing everything, flip the display
-    pygame.display.flip()
+            # *after* drawing everything, flip the display
+            pygame.display.flip()
+
 
 pygame.quit()
